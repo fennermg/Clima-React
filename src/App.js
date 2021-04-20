38be94e2b520cdbd0117753e5c2689396f82f7ef
Fenner Mena <fmenagaray@gmail.com>
2021-04-19 23:32:14 -0600

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Fragment, useState, useEffect } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import Clima from "./components/Clima";
+import Error from "./components/Error";
+
+function App() {
+  //State del formulario
+
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  const { ciudad, pais } = busqueda;
+
+  const [consultar, guardarConsultar] = useState(false);
+
+  const [resultado, guardarResultado] = useState({});
+
+  const [error, guardarError] = useState(false);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (consultar) {
+        const appId = "9af9dbb54b1bcb40efaf35981e6826e4";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        
+        guardarError(resultado.cod === "404");
+        guardarResultado(resultado);
+        guardarConsultar(false);
+
+        //Verificar resultados correctos
+      }
+    };
+    consultarAPI();
+  }, [consultar]);
+
+  //let componente;
+
+  // if (error) {
+  //   componente = <Error mensaje="No hay resultados" />;
+  // } else {
+  //   componente = <Clima resultado={resultado} />;
+  // }
+
+  return (
+    <Fragment>
+      <Header titulo="Clima React App" />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              />
+            </div>
+            <div className="col m6 s12">
+              {error ? (
+                <Error mensaje="No hay resultados" />
+              ) : (
+                <Clima resultado={resultado} />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Clima.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+
+
+const Clima = ({resultado}) => {
+
+    //extraer los valores
+    
+    const {name, main} = resultado;
+
+    if (!name) {
+        return null;
+    }
+
+    //Grados 
+
+    const kelvin = 273.15;
+
+    return ( 
+
+        <div className="card-panel white col s12">
+            <div className="black-text">
+                <h2>El clima de {name} es:</h2>
+                <p className="temperatura">{parseFloat(main.temp-kelvin,10).toFixed(2)} <span>&#x2103;</span></p>
+
+                <p className="">Temperatura maxima: {parseFloat(main.temp_max-kelvin,10).toFixed(2)} <span>&#x2103;</span></p>
+                
+                <p className="">Temperatura minima: {parseFloat(main.temp_min-kelvin,10).toFixed(2)} <span>&#x2103;</span></p>
+            </div>
+        </div>
+
+     );
+}
+ 
+export default Clima;
